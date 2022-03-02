d358bc8610287a27efd41b1c8340e0818ad5aae5
Andrés Cañón <mario.canon@datatools.com.co>
2022-03-02 16:31:36 -0500

    Initial commit

---
+++ src/main/java/com/ms/app/examsmicroservice/ExamsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ms.app.examsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/app/examsmicroservice/ExamsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.app.examsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
